113fe0601ea67551a53cfffc0cceb4d020c72095
Ammar <maansaridot@gmail.com>
2020-04-24 20:39:05 +0500

    firebase function with storage bucket for image

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,19 +2,30 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
   return (dispatch) => {
-    const placeData = {
-      name: placeName,
-      location,
-    };
-
-    fetch('https://rn-course-v2.firebaseio.com/places.json', {
+    fetch('https://us-central1-rn-course-v2.cloudfunctions.net/storeImage', {
       method: 'POST',
-      body: JSON.stringify(placeData),
+      body: JSON.stringify({
+        image: image.base64,
+      }),
     })
       .catch((err) => console.log(err))
       .then((res) => res.json())
       .then((parsedRes) => {
-        console.log(parsedRes);
+        console.log('Image stored', parsedRes);
+        const placeData = {
+          name: placeName,
+          location,
+          image: parsedRes.imageUrl,
+        };
+        return fetch('https://rn-course-v2.firebaseio.com/places.json', {
+          method: 'POST',
+          body: JSON.stringify(placeData),
+        });
+      })
+      .catch((err) => console.log(err))
+      .then((res) => res.json())
+      .then((parsedRes) => {
+        console.log('Data stored', parsedRes);
       });
   };
 };
